53fce37bd20c305dde9a592b700a40f7715bdb3f
Natan Lieberman <natanlieberman@gmail.com>
2019-12-16 13:37:49 +0200

    added

--- src/app/comps/office/office.component.ts
+++ src/app/comps/office/office.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfficeComponent implements OnInit {
 
-  call_maneger = 0
-  call_secretary = 0
-  call_worker = 0
+  call_maneger1 = 0
+  call_secretary1 = 0
+  call_worker1 = 0
+  
+  call_maneger2 = 0
+  call_secretary2 = 0
+  call_worker2 = 0
+
+  call_maneger3 = 0
+  call_secretary3 = 0
+  call_worker3 = 0
+  
+  call_maneger4 = 0
+  call_secretary4 = 0
+  call_worker4 = 0
+  
+  call_maneger5 = 0
+  call_secretary5 = 0
+  call_worker5 = 0
+  
+  call_maneger6 = 0
+  call_secretary6 = 0
+  call_worker6 = 0
+  
+  call_maneger7 = 0
+  call_secretary7 = 0
+  call_worker7 = 0
+  
+  call_maneger8 = 0
+  call_secretary8 = 0
+  call_worker8 = 0
+  
+  call_maneger9 = 0
+  call_secretary9 = 0
+  call_worker9 = 0
+  
+  call_maneger10 = 0
+  call_secretary10 = 0
+  call_worker10 = 0
+  
+  call_maneger11 = 0
+  call_secretary11 = 0
+  call_worker11 = 0
   
 
   constructor() { }
